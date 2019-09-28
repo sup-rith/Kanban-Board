0866bb52e0a2ded3a04fd9c1199c3f8265308e61
Suprith Aireddy <aireddysuprith@gmail.com>
2019-09-28 15:00:57 -0500

    Added Firebase to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+    //Firebase Credentials
+    firebaseConfig: {
+      apiKey: "AIzaSyAZp-FuPVb3Vu07VboQ5ebhVGp1J-a_CPs",
+      authDomain: "kanban-3b5d1.firebaseapp.com",
+      databaseURL: "https://kanban-3b5d1.firebaseio.com",
+      projectId: "kanban-3b5d1",
+      storageBucket: "",
+      messagingSenderId: "1054336743999",
+      appId: "1:1054336743999:web:1dd36e07db2695d26fd68a"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  //Firebase Credentials
+  firebaseConfig: {
+    apiKey: "AIzaSyAZp-FuPVb3Vu07VboQ5ebhVGp1J-a_CPs",
+    authDomain: "kanban-3b5d1.firebaseapp.com",
+    databaseURL: "https://kanban-3b5d1.firebaseio.com",
+    projectId: "kanban-3b5d1",
+    storageBucket: "",
+    messagingSenderId: "1054336743999",
+    appId: "1:1054336743999:web:1dd36e07db2695d26fd68a"
+  }
 };
 
 /*
