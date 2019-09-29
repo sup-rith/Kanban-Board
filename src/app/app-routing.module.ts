c8e89f93d79583b4bd3c43770fb52b1fcd27afd0
Suprith Aireddy <aireddysuprith@gmail.com>
2019-09-29 09:15:04 -0500

    Google login and Updated styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 
 const routes: Routes = [
-  {
-    path: '' , component: HomePageComponent
-  }
+  { path: '' , component: HomePageComponent },
+  //Lazy loading
+  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }
 ];
 
 @NgModule({
